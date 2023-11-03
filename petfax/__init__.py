d8f3d0749ed3e51213472a45e94a8b1a9d82e8ae
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-11-03 00:48:45 -0400

    static files & styling - attempted the template inheritance

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,20 +1,29 @@
 from flask import Flask
+from flask_migrate import Migrate
 
+# factory
 def create_app(): 
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+    from . import models 
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
+    # index route
     @app.route('/')
-    def hello(): 
+    def index(): 
         return 'Hello, PetFax!'
 
-    # pets index route
-    @app.route('/pets')
-    def pets(): 
-        return 'These are our pets available for adoption!'
-
     # register pet blueprint 
-    from . import pet
+    from . import pet 
     app.register_blueprint(pet.bp)
+
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
 
     # return the app 
     return app
